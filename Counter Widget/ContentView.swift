317ef41883da78c54a34f88e6560600a5723ac6f
YJ Soon <yjsoon@tinkertanker.com>
2023-09-21 19:45:10 +0800

    Initial Commit

---
+++ Counter Widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Counter Widget
+//
+//  Created by YJ Soon on 21/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Counter Widget/Counter_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_WidgetApp.swift
+//  Counter Widget
+//
+//  Created by YJ Soon on 21/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Counter_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
